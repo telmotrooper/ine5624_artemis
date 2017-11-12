3b875e221d8a9d5a38dee55cf512adef5c9bf4db
Telmo Trooper <telmo.trooper@gmail.com>
2017-11-12 19:34:56 -0200

    Pictures page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res, next) {
-	res.render("index", {
-		title: "Totem"
-	});
+	res.render("index");
 });
 
 module.exports = router;
