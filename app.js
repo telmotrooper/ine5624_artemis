a443dab64419e357a1b7584aa0c00f3baabaa058
Telmo Trooper <telmo.trooper@gmail.com>
2017-11-12 20:21:16 -0200

    Added payment page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 
 const index = require("./routes/index");
 const input_method = require("./routes/input_method");
+const payment = require("./routes/payment");
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", index);
 app.use("/input_method", input_method);
+app.use("/payment", payment);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
